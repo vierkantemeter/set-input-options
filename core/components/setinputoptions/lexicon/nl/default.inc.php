13484ac66266767415c6d807967753ea10af3963
Vierkante Meter <mark@vierkante-meter.nl>
2016-01-21 12:27:11 +0100

    Update default.inc.php

--- core/components/setinputoptions/lexicon/nl/default.inc.php
+++ core/components/setinputoptions/lexicon/nl/default.inc.php
@@ -1,51 +1,51 @@
 <?php
 /**
- * Default English Lexicon Entries for SetInputOptions
+ * Dutch Lexicon Entries for SetInputOptions
  *
  * @package setinputoptions
  * @subpackage lexicon
  */
 
-$_lang['setinputoptions'] = 'Set Input Options';
+$_lang['setinputoptions'] = 'Invoeropties beheren';
 
-$_lang['setinputoptions.menu.setinputoptions'] = 'Set Input Options';
-$_lang['setinputoptions.menu.setinputoptions_desc'] = 'Add input options values for your tv\'s';
+$_lang['setinputoptions.menu.setinputoptions'] = 'Invoeropties';
+$_lang['setinputoptions.menu.setinputoptions_desc'] = 'Voeg invoeropties toe voor tv\'s.';
 
-$_lang['setinputoptions.global.search'] = 'Search';
+$_lang['setinputoptions.global.search'] = 'Zoeken';
 
-$_lang['setinputoptions.group.groups'] = 'Groups';
-$_lang['setinputoptions.group.intro_msg'] = 'Manage your groups.';
+$_lang['setinputoptions.group.groups'] = 'Groepen';
+$_lang['setinputoptions.group.intro_msg'] = 'Groepen beheren.';
 
-$_lang['setinputoptions.group.name'] = 'Name';
-$_lang['setinputoptions.group.description'] = 'Create group';
+$_lang['setinputoptions.group.name'] = 'Naam';
+$_lang['setinputoptions.group.description'] = 'Groep maken';
 $_lang['setinputoptions.group.position'] = 'Position';
-$_lang['setinputoptions.group.create'] = 'Create a group';
-$_lang['setinputoptions.group.update'] = 'Update a group';
-$_lang['setinputoptions.group.edit_options'] = 'Edit options';
-$_lang['setinputoptions.group.remove'] = 'Remove a group';
-$_lang['setinputoptions.group.remove_confirm'] = 'Are you sure you want to remove this group?';
+$_lang['setinputoptions.group.create'] = 'Groep maken';
+$_lang['setinputoptions.group.update'] = 'Groep bewerken';
+$_lang['setinputoptions.group.edit_options'] = 'Invoeropties bewerken';
+$_lang['setinputoptions.group.remove'] = 'Groep verwijderen';
+$_lang['setinputoptions.group.remove_confirm'] = 'Weet je zeker dat je deze groep wilt verwijderen?';
 
-$_lang['setinputoptions.err.group_name_ae'] = 'An group already exists with that name.';
-$_lang['setinputoptions.err.group_nf'] = 'Group not found.';
-$_lang['setinputoptions.err.group_name_ns'] = 'Name is not specified.';
-$_lang['setinputoptions.err.group_remove'] = 'An error occurred while trying to remove the group.';
-$_lang['setinputoptions.err.group_save'] = 'An error occurred while trying to save the group.';
+$_lang['setinputoptions.err.group_name_ae'] = 'Er bestaat al een groep met deze naam.';
+$_lang['setinputoptions.err.group_nf'] = 'Groep kan niet worden gevonden.';
+$_lang['setinputoptions.err.group_name_ns'] = 'Er is geen naam opgegeven.';
+$_lang['setinputoptions.err.group_remove'] = 'Er is een fout opgetreden bij het verwijderen van de groep.';
+$_lang['setinputoptions.err.group_save'] = 'Er is een fout opgetreden bij het opslaan van de groep.';
 
-$_lang['setinputoptions.inputoption.inputoption'] = 'Input options';
-$_lang['setinputoptions.inputoption.intro_msg'] = 'Manage your input options for the group: ';
+$_lang['setinputoptions.inputoption.inputoption'] = 'Invoeropties';
+$_lang['setinputoptions.inputoption.intro_msg'] = 'Beheer de invoeropties voor groep: ';
 
-$_lang['setinputoptions.inputoption.name'] = "Name";
+$_lang['setinputoptions.inputoption.name'] = "Naam";
 $_lang['setinputoptions.inputoption.alias'] = "Alias";
-$_lang['setinputoptions.inputoption.goBack'] = "Back to groups";
+$_lang['setinputoptions.inputoption.goBack'] = "Terug naar alle groepen";
 $_lang['setinputoptions.inputoption.type'] = "Type";
-$_lang['setinputoptions.inputoption.position'] = "Position";
-$_lang['setinputoptions.inputoption.export'] = 'Export';
-$_lang['setinputoptions.inputoption.exportMsg'] = 'After clicking Yes, we will prepare a CSV export of the input options in this group. This export can be used to import these input options in another group or on another installation. It can take a few seconds if you\'ve got a lot of input options, please be patient.';
-$_lang['setinputoptions.inputoption.create'] = 'Add an input option';
-$_lang['setinputoptions.inputoption.update'] = 'Update an input option';
-$_lang['setinputoptions.inputoption.edit_options'] = 'Edit input option';
-$_lang['setinputoptions.inputoption.remove'] = 'Remove this input option';
-$_lang['setinputoptions.inputoption.remove_confirm'] = 'Are you sure you want to remove this input option?';
-$_lang['setinputoptions.inputoption.duplicate'] = 'Duplicate this input option';
-$_lang['setinputoptions.inputoption.duplicate_confirm'] = 'Are you sure you want to duplicate this input option?';
-$_lang['setinputoptions.err.inputoption.save'] = 'An error has occured while trying to save the input option.';
+$_lang['setinputoptions.inputoption.position'] = "Positie";
+$_lang['setinputoptions.inputoption.export'] = 'Exporteren';
+$_lang['setinputoptions.inputoption.exportMsg'] = 'Als je op \'Ja\' klikt, wordt er een CSV-bestand gegenereerd met daarin alle invoeropties uit deze groep. Met dit bestand kun je de invoeropties in andere groepen importeren. Het genereren van het bestand kan even duren als je veel invoeropties hebt.';
+$_lang['setinputoptions.inputoption.create'] = 'Invoeroptie toevoegen';
+$_lang['setinputoptions.inputoption.update'] = 'Invoeroptie bewerken';
+$_lang['setinputoptions.inputoption.edit_options'] = 'Invoeroptie bewerken';
+$_lang['setinputoptions.inputoption.remove'] = 'Invoeroptie verwijderen';
+$_lang['setinputoptions.inputoption.remove_confirm'] = 'Weet je zeker dat je deze invoeroptie wilt verwijderen?';
+$_lang['setinputoptions.inputoption.duplicate'] = 'Invoeroptie dupliceren';
+$_lang['setinputoptions.inputoption.duplicate_confirm'] = 'Weet je zeker dat je deze invoeroptie wilt dupliceren?';
+$_lang['setinputoptions.err.inputoption.save'] = 'Er is een fout opgetreden bij het opslaan van de invoeroptie.';
